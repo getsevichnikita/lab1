c6668f8fbbb1a330bf5bcf4f0772f9705a4ef1b6
nikit <getsevich.n@gmail.com>
2026-05-28 23:11:25 +0300

    lr8 change

--- library-frontend/vite.config.js
+++ library-frontend/vite.config.js
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
+  base: '/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  resolve: {
+    alias: {
+      '/src': '/opt/render/project/src/library-frontend/src',
+    },
   },
 });
