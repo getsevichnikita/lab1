4e0a88aee625bde12f15363ed112c56510725c3f
nikit <getsevich.n@gmail.com>
2026-05-29 11:29:53 +0300

    CORS change

--- src/main/java/com/library/config/CorsConfig.java
+++ src/main/java/com/library/config/CorsConfig.java
@@ -2,36 +2,27 @@ package com.library.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation
-        .CorsRegistry;
-import org.springframework.web.servlet.config.annotation
-        .WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
-
             @Override
-            public void addCorsMappings(
-                    CorsRegistry registry
-            ) {
-
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173"
+                                "http://localhost:5173",
+                                "http://localhost:3000",
+                                "https://liberlibrary.onrender.com"
                         )
-                        .allowedMethods(
-                                "*"
-                        )
-                        .allowedHeaders(
-                                "*"
-                        );
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
 }
-
